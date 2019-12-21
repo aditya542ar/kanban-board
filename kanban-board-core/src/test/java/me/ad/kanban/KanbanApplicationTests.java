d6cdf03ecfe9d07ecee0f159961eff8110382b38
Aditya <aditya542ar@gmail.com>
2019-12-21 17:12:52 +0530

    Initial project structure setup

---
+++ kanban-board-core/src/test/java/me/ad/kanban/KanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ad.kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
