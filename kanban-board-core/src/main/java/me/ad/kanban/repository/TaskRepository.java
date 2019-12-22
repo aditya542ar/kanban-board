9473b05f09c8ab4a96161acefac3a6a4a650967e
Aditya <aditya542ar@gmail.com>
2019-12-22 12:54:47 +0530

    Created Repositories. Closes #2

---
+++ kanban-board-core/src/main/java/me/ad/kanban/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package me.ad.kanban.repository;
+
+import me.ad.kanban.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
